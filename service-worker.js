ad8c6fcc20011eed57213beb5ad92e8900bb634f
daniel <modestoangelica9@gmail.com>
2025-04-08 12:13:23 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -35,3 +35,24 @@ self.addEventListener('fetch', event => {
     })
   );
 });
+self.addEventListener('install', function(event) {
+  console.log('Service Worker instalado');
+  event.waitUntil(
+    caches.open('app-cache').then(function(cache) {
+      return cache.addAll([
+        '/app/index.html',
+        '/app/manifest.json',
+        '/app/logo192.png'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
+
