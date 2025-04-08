596cbe8b898b3acb33263e1fc500ad69cdd15732
daniel <modestoangelica9@gmail.com>
2025-04-08 11:34:47 -0300

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = 'cardapio-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './logo192.png',
+  './logo512.png'
+];
+
+// Instala o Service Worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Ativa o Service Worker
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+      );
+    })
+  );
+});
+
+// Intercepta requisições
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
