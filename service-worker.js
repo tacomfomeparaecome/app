a84afe7e7efc70144a87930f1966c8154d343a77
daniel <modestoangelica9@gmail.com>
2025-04-08 12:15:38 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,40 +1,3 @@
-const CACHE_NAME = 'cardapio-v1';
-const urlsToCache = [
-  './',
-  './index.html',
-  './manifest.json',
-  './logo192.png',
-  './logo512.png'
-];
-
-// Instala o Service Worker
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
-  );
-});
-
-// Ativa o Service Worker
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(
-        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
-      );
-    })
-  );
-});
-
-// Intercepta requisições
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
-});
 self.addEventListener('install', function(event) {
   console.log('Service Worker instalado');
   event.waitUntil(
@@ -55,4 +18,3 @@ self.addEventListener('fetch', function(event) {
     })
   );
 });
-
